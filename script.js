3eb46cce51b1a4b40828f1b5c7c324262941a2fd
idan hamo <idanh19999@gmail.com>
2022-03-03 16:48:42 +0200

    .

--- script.js
+++ script.js
@@ -1,50 +1,47 @@
-const divy = document.getElementById("trivia")
+const divy = document.getElementById("trivia");
+
 const questArr = [
-    {
-        questionId:1,
-        question : "how are you?",
-    },
-    {
-        questionId:2,
-        question : "how old are you?",
+  {
+    questionId: 1,
+    question: "how are you?",
+  },
+  {
+    questionId: 2,
+    question: "how old are you?",
+  },
+  {
+    questionId: 3,
+    question: "alooooo?",
+  },
+  {
+    questionId: 4,
+    question: "fgsgo?",
+  },
+  {
+    questionId: 5,
+    question: "v456b5464b?",
+  },
+  {
+    questionId: 6,
+    question: "sfdgshs6seg?",
+  },
+];
 
-    },
-    {
-        questionId:3,
-        question : "alooooo?",
-    },];
-
-
-    const answerArr = [
-        {
-        answers: [1,2,3,4]
-        },
-        {
-        answers: [1,2,3,4]
-        },
-        {
-        answers: [1,2,3,4]
-        },
-
-    ];
-
+const answerArr = [
+  { answers: ["sagi", "idan", "shalom", "daniel"] },
+  { answers: [2, 3, 4, 5] },
+  { answers: [1, 2, 3, 4] },
+  { answers: [1, 2, 3, 4] },
+  { answers: [1, 2, 3, 4] },
+  { answers: [1, 2, 3, 4] },
+];
 
 for (const quest of questArr) {
-    let html = `<br><label for="question${quest.questionId}">question ${quest.questionId}</label><br>`;
-    for (let i = 0; i < 4; i++) {
-        const answer = answerArr[i];
-        console.log(answer.answers);
-        for (const value of answer.answers) {
-            html+= `<input type="radio" name="question ${quest.questionId}">`
-            html += `<span>${value}</span>`
-            
-        }
+  let html = `<br><label for="question${quest.questionId}">question ${quest.questionId}</label><br>`;
+  for (const key in answerArr) {
+    html += `<input type="radio" name="question${quest.questionId}">`;
+    html += `<span>${answerArr[key].answers}</span>`;
+  }
 
-        
-    }
-            
-        
-
-    divy.innerHTML += html
-
+  divy.innerHTML += html;
 }
